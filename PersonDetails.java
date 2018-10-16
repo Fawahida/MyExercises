dbc8b305e1b3e2f41b509921011aa1a877be285d
Fawahida <43943016+Fawahida@users.noreply.github.com>
2018-10-16 15:34:13 +0800

    Ex2.1

---
+++ PersonDetails.java
@@ -0,0 +1,20 @@
+import org.apache.log4j.Logger;
+
+
+public class PersonDetails {
+	
+	static Logger log = Logger.getLogger(PersonDetails.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.info("First Name : diviya");
+		log.info("Last Name : Bharathi");
+		log.info("Gender: F");
+		log.info("Age:20");
+		log.info("Weight: 85.55");
+
+		
+	}
+
+}
