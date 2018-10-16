9b0613c674b89c4621ce75058bfb2def0c346cef
Fawahida <43943016+Fawahida@users.noreply.github.com>
2018-10-16 17:07:12 +0800

    Ex3.1

---
+++ StringOperation.java
@@ -0,0 +1,102 @@
+
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+import com.capgemini.utils.Logging;
+import com.capgemini.utils.keyboard;
+
+public class StringOperation {
+	
+	private static final String menu[] = {"Add String to itself" , 
+		   "Replace odd positions with #",
+		   "Remove duplicate characters in the String",
+		   "Change odd characters to upper case"};
+	
+	private String string;
+	
+	public void run ()
+	{
+		string = keyboard.readString ("Enter a string : ");	// Get string from user
+		int choice = -1;
+		
+		while (choice != 0)
+		{
+			choice = keyboard.menuInput ("Select an option", menu); // User menu to ask user choice
+			
+			switch (choice)
+			{
+				case 1:
+					addString ();
+					break;
+				
+				case 2:
+					replaceOdd ();
+					break;
+					
+				case 3:
+					removeDuplicate ();
+					break;
+				
+				case 4:
+					changeOdd ();
+			}
+			
+			if (choice != 0)
+			{
+				Logging.LOG.info (string);
+				Logging.LOG.info ("");
+			}
+		}
+	}
+	
+	public void addString ()	// Function to concatenate string with itself
+	{
+		string += string;
+	}
+	
+	public void replaceOdd ()
+	{
+		char charArr [] = string.toCharArray ();		// Convert string to char array
+		
+		for (int i = 1; i < charArr.length; i += 2)		// Loop through odd chars and replace with #
+			
+			charArr [i] = '#';
+		
+		string = new String (charArr);					// Convert charArray to string
+	}
+	
+	public void removeDuplicate ()
+	{
+		char charArray [] = string.toCharArray ();							// Convert string to char array
+		LinkedHashSet <Character> set = new LinkedHashSet <Character> ();
+		
+		for (char c : charArray) set.add (c);								// Add all char into set to remove duplicates
+		
+		StringBuilder sb = new StringBuilder ();							// Append all chars using string builder
+		for (Character c : set)
+			
+			sb.append (c);
+		
+		string = sb.toString ();											// Convert back to string
+	}
+	
+	public void changeOdd ()
+	{
+		char charArr [] = string.toCharArray ();				// Convert string to char array
+		
+		for (int i = 1; i < charArr.length; i += 2)				// Use Character class to turn odd char to uppercase
+		
+			charArr [i] = Character.toUpperCase (charArr [i]);
+		
+		string = new String (charArr);							// Convert char array back to string
+	}
+	
+	public static void main (String [] args)
+	{
+		new StringOperation ().run ();
+	}
+	
+}
